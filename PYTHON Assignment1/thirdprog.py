c4c3785b53569459648ec36f88af37547aaad698
Muskan <muskantaneja85@gmail.com>
2025-06-27 10:47:42 +0530

    Update thirdprog.py

--- PYTHON Assignment1/thirdprog.py
+++ PYTHON Assignment1/thirdprog.py
@@ -1,3 +1,4 @@
+#find quotient and remainder
 firstNum=int(input("enter first number: "))
 secondNum=int(input("enter second number: "))
 print("quotient of ",firstNum ,"dividing by",secondNum, ": " , firstNum/secondNum)  #we can use // for ans without point
