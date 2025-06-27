64767dcb5da31906e1e6fa2a3f5a8b5e590d5933
Muskan <muskantaneja85@gmail.com>
2025-06-27 10:46:25 +0530

    Update firstprog.py

--- PYTHON Assignment1/firstprog.py
+++ PYTHON Assignment1/firstprog.py
@@ -1,6 +1,7 @@
+#calculate percentage of 5 subject
 english=int(input("enter marks in english: "))
 hindi=int(input("enter marks in hindi: "))
 maths=int(input("enter marks in maths: "))
 sst=int(input("enter marks in ss: "))
 science=int(input("enter marks in science: "))
-print("percentage:",(english+hindi+maths+sst+science)/500*100)66
+print("percentage:",(english+hindi+maths+sst+science)/500*100)
