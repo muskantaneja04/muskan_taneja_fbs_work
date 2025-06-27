f5009ca4401caf2232c91480ec4bb0f90ba8ddce
Muskan <muskantaneja85@gmail.com>
2025-06-27 10:52:46 +0530

    Update ninthprog.py

--- PYTHON Assignment1/ninthprog.py
+++ PYTHON Assignment1/ninthprog.py
@@ -1,3 +1,4 @@
+#find area of triangle from base and height.
 base=int(input("enter base of triangle : "))
 height=int(input("enter height of triangle : "))
 print("area of triangle : ",int(1/2*base*height))  #ans int mei ayega instead of float ,float mei 1/2 ki vjah se tha
